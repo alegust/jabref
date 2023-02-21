f0fbd58258ffbb3bcc6b51d1cba29c58d9a546ee
alegust <61827827+alegust@users.noreply.github.com>
2023-02-21 01:28:48 +0100

    Adds two new tests

--- src/test/java/org/jabref/logic/importer/fileformat/BiblioscapeImporterTest.java
+++ src/test/java/org/jabref/logic/importer/fileformat/BiblioscapeImporterTest.java
@@ -1,16 +1,24 @@
 package org.jabref.logic.importer.fileformat;
 
 import org.jabref.DIYcoverage.DIYCoverage; /*ASSI3: Extra class for coverage*/
+
+import java.io.*;
+import java.net.URL;
 import java.nio.file.Path;
 import java.util.Collections;
+import java.util.List;
 
+import org.jabref.logic.bibtex.BibEntryAssert;
+import org.jabref.logic.importer.ParserResult;
+import org.jabref.logic.importer.fetcher.GvkParserTest;
 import org.jabref.logic.util.StandardFileType;
 
+import org.jabref.model.entry.BibEntry;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BiblioscapeImporterTest {
 
@@ -49,11 +57,33 @@ public class BiblioscapeImporterTest {
                 importer.importDatabase(file).getDatabase().getEntries());
         for(int k = 0; k < importer.taken.length; k++){ /*ASSI3: For branch coverage DIY*/
             if(importer.taken[k]) {
-                DIYCoverage.takenTest[0][k] = true;
+                DIYCoverage.takenTest[2][k] = true;
             }
         }
     }
 
+    @Test
+    public void testLoadFileWithQAExtension() throws Throwable{
+        assertEquals(ParserResult.class,importer.importDatabase(new BufferedReader(new FileReader("src/test/resources/test.QA"))).getClass());
+        for(int k = 0; k < importer.taken.length; k++){ /*ASSI3: For branch coverage DIY*/
+            if(importer.taken[k]) {
+                DIYCoverage.takenTest[2][k] = true;
+            }
+        }
+    }
+
+    @Test
+    public void testOneReference() throws Throwable{
+        assertEquals(Collections.emptyList(),
+                importer.importDatabase(new BufferedReader(new FileReader("src/test/resources/testfile.bib"))).getDatabase().getEntries());
+
+        for(int k = 0; k < importer.taken.length; k++){ /*ASSI3: For branch coverage DIY*/
+            if(importer.taken[k]) {
+                DIYCoverage.takenTest[2][k] = true;
+            }
+        }
+    }
+    
     @AfterAll
     public static void printCoverage(){
         DIYCoverage.printAllTrue();
